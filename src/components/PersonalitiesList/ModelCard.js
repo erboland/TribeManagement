62a375e1cae937c3888384a03874106ae5c8319d
erboland <karshigaerbol@gmail.com>
2019-06-18 15:09:05 +0600

    fix try 2

--- src/components/PersonalitiesList/ModelCard.js
+++ src/components/PersonalitiesList/ModelCard.js
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom';
 const ModelCard = ({modelName, modelHeight, modelChest, modelWaist, modelShoes, modelHair, modelEyes, id, mainImg, isCasting, modelHips, modelAs}) =>{ 
     
     let castingWidthClass=isCasting?'hoverBox fl w-100 w-50-m w-33-l pa2':'hoverBox fl w-100 w-50-m w-25-l pa2';
-    let image=mainImg?mainImg:'http://is3.mzstatic.com/image/thumb/Music49/v4/b6/b0/a1/b6b0a1dd-998d-9786-ca2f-87470be15250/source/400x40000bb.png';
+    let image=mainImg?mainImg:'https://cdn1.iconfinder.com/data/icons/user-pictures/100/unknown-512.png';
     let modelType= modelAs?modelAs: '';
     let onClickFunction=()=>{
         return true
@@ -14,7 +14,7 @@ const ModelCard = ({modelName, modelHeight, modelChest, modelWaist, modelShoes, 
                     
                     <a className={castingWidthClass} onClick={onClickFunction()}> 
                         
-                            <img alt='Model Image' className='hoverBox_layer_bottom w-100 db outline black-10' id='image' src={image} onHov />
+                            <img alt='Model Image' className='hoverBox_layer_bottom w-100 db outline black-10' id='image' src={image} />
                             <div className='hoverBox_layer_top'>
                                 <div className='hoverBox_text'>
                                     <p className='b pt0 f4 pb0'>
